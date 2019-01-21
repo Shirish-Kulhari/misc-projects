98052cbff04a7695ca91c28e4d6d854a9aa170bf
Shirish-Kulhari <kulhari.shirish@gmail.com>
2019-01-21 19:22:55 +0530

    Updated main program (more optimized)
    Thanks to Quuxplusone (https://codereview.stackexchange.com/users/16369/quuxplusone) for several suggestions, including limiting the denominator of fractions.
    See: https://codereview.stackexchange.com/questions/211882/simulating-a-two-body-collision-problem-to-find-digits-of-pi
    In addition, replaced Block class by 3 simple lists, added support for user input and input validation and added docstrings.

--- TwoBodyCollision/TwoBodyCollision.py
+++ TwoBodyCollision/TwoBodyCollision.py
@@ -1,100 +1,162 @@
+"""
+See the simulate() function for the main docstring
+"""
+
 from fractions import Fraction
 
-class Block:
-  # Creating a class since that helps keep track of attributes
-  def __init__(self, mass, velocity, position):
-    self.mass = mass
-    self.velocity = velocity
-    self.position = position
-
-# By "collision", we mean that either the position of both the objects is the
-# same (both collide against each other) or the position of the small block is
-# 0 (collision against wall)
- 
-def updateVelocities(small, large, collisions):
-  if(small.position == large.position != 0):
-    # Both blocks collide against each other
-    collisions += 1
-    temp = small.velocity
+def updateVelocities(mass, velocity, position):
+  """
+  Looks at the current configuration of the blocks and updates velocities.
   
-    small.velocity = Fraction(((2*large.mass*large.velocity)+
-    (small.mass*small.velocity)-(large.mass*small.velocity)),
-    (small.mass + large.mass))
+  By "collision", we mean that either the position of both the objects is the
+  same (both collide against each other) or the position of the small block is
+  0 (collision against wall)
+  """
+  if(position[0] == position[1] != 0):
+    # Both blocks collide against each other
+    temp = velocity[0]
+  
+    velocity[0] = Fraction(((2*mass[1]*velocity[1])+(mass[0]*velocity[0])-
+                  (mass[1]*velocity[0])), (mass[0] + mass[1]))
     
-    large.velocity = Fraction(((2*small.mass*temp)+(large.mass*large.velocity)
-    -(small.mass*large.velocity)),(small.mass + large.mass))
+    velocity[1] = Fraction(((2*mass[0]*temp)+(mass[1]*velocity[1])-
+                  (mass[0]*velocity[1])), (mass[0] + mass[1]))
     
-  elif(small.position == 0 != large.position):
+  elif(position[0] == 0 != position[1]):
     # The small block gets "reflected" off the wall
-    collisions += 1
-    small.velocity = -small.velocity
+    velocity[0] = Fraction(-velocity[0], 1)
     
-  elif(small.position == large.position == 0):
+  elif(position[0] == position[1] == 0):
     # The rare instance in which both blocks move towards the wall and
     # collide with the wall and against each other simultaneously
-    collisions += 2
-    small.velocity, large.velocity = -small.velocity, -large.velocity
+    velocity[0], velocity[1] = Fraction(-velocity[0], 1), Fraction(
+                              -velocity[1], 1)
   else:
     pass
-  
-  return collisions
 
-# Given the current positions and velocities, find the time to next collision
-# This takes care of all different scenarios  
-def timeToNextCollision(small, large):
-  if(large.velocity >= small.velocity >= 0):
+
+def timeToNextCollision(velocity, position):
+  """
+  Given the current positions and velocities, find the time to next collision.
+  """
+  if(velocity[1] >= velocity[0] >= 0):
     # Both blocks move towards right, but the large block is faster and the
     # small block can't catch up
-    return float("inf")
+    return -1
   
-  elif(small.velocity >= 0 >= large.velocity):
+  elif(velocity[0] >= 0 >= velocity[1]):
     # Both blocks are either moving towards each other, or one of the is at
     # rest and the other is moving towards it. The wall is obviously ignored
-    # The condition small.velocity == 0 == large.velocity will also be ignored
+    # The condition velocity[0] == 0 == velocity[1] will also be ignored
     # since if that's true, only the first if statement would be executed.
-    return Fraction(large.position - small.position,
-                    small.velocity - large.velocity)
+    return Fraction(position[1] - position[0],
+                    velocity[0] - velocity[1])
   
-  elif((large.velocity >= 0 and small.velocity < 0) or
-       (small.velocity <= large.velocity < 0)):
+  elif((velocity[1] >= 0 > velocity[0]) or (velocity[0] <= velocity[1] < 0)):
     # Both blocks move towards left, but the large block can't catch up with
     # the small block before the latter runs into the wall
-    return Fraction(-small.position, small.velocity)
+    return Fraction(-position[0], velocity[0])
   
-  elif(small.position == 0):
+  elif(position[0] == 0):
     # Special case for when the small block is currently at the wall
-    if(large.velocity >= abs(small.velocity)):
+    if(velocity[1] >= abs(velocity[0])):
       # Small block can no longer catch up with large block
-      return float("inf")
+      return -1
     else:
       # Large block is either moving towards left or too slow moving towards
       # the right. In either case, they will collide
-      return large.position/(abs(small.velocity) - large.velocity)
+      return Fraction(position[1], (abs(velocity[0]) - velocity[1]))
   else:
     # Both blocks move towards left, but large block is faster. If the
     # distance between blocks is small enough compared to that between the wall
     # and the small block, they will collide. Otherwise the small block will
     # reach the wall before the large block has a chance to catch up
-    return min(Fraction(-small.position, small.velocity),
-               Fraction(large.position - small.position), 
-                       (small.velocity - large.velocity))
+    return min(Fraction(-position[0], velocity[0]),
+               Fraction((position[1] - position[0]), 
+                       (velocity[0] - velocity[1])))
     
+def getInputs():
+  """ Receives inputs specifying initial conditions. """
+  print("\nPlease enter INTEGER VALUES ONLY (negative values of mass will")
+  print("be automatically converted to positive). Default values:\n")
+  print("Small mass: 1. Large mass: 10000.\nSmall block velocity = ", end = "")
+  print("0. Large block velocity = -1.\nSmall block position = 1.", end = "")
+  print(" Large block position = 2.")
+  str = " [<Enter> to use default value]: "
+  try:
+    mass = [abs(int(input("Mass of smaller block"+str) or "1")), 
+            abs(int(input("Mass of larger block"+str) or "10000"))]
+    velocity = [int(input("Velocity of smaller block"+str)
+            or "0"), int(input("Velocity of larger block"+str) or "-1")]
+    position = [int(input("Position of smaller block"+str)
+            or "1"), int(input("Position of larger block"+str) or "2")]
+    return mass, velocity, position
+  except:
+    return [], [], []
 
-def simulate(small, large):
+def simulate():
+  """
+  Based on the following video by 3Blue1Brown:
+  https://www.youtube.com/watch?v=HEfHFsfGXjs
+  
+  Simulates a two-body collision to approximate the value of Pi. Consider the
+  positive x-axis as a frictionless floor and the positive y-axis as a wall.
+  
+  Given two blocks at y=0 with user-specified masses, initial x-axis positions 
+  and initial horizontal velocities, this function finds the total number of
+  perfectly elastic collisions that can happen.
+  
+  If:
+    0 < small block's position < large block's position,
+    the small block has UNIT mass, 
+    the large block's mass equals the n-th power of 100,
+    the large block has a sufficiently large velocity towards the left with
+    respect to the small block,
+    
+  then the total number of collisions should equal floor(Pi*(10^n)).
+  
+  Thanks to Quuxplusone from Code Review stackexchange for suggesting a
+  substantial performance improvement as well as helpful tweaks.
+
+  [ https://codereview.stackexchange.com/users/16369/quuxplusone ]
+  [ https://codereview.stackexchange.com/questions/211882/
+   simulating-a-two-body-collision-problem-to-find-digits-of-pi ]
+  """
+  mass, velocity, position = getInputs()
+  if(not(mass and velocity and position)):
+    print("Invalid input(s). Exiting...")
+    return
   collisionCount = 0
+  
+  print("\nPlease wait...")
   while True:
-    t = timeToNextCollision(small, large)
-    if(t == float("inf")):
+    t = timeToNextCollision(velocity, position)
+    if(t == -1):
       # No more collisions
       break
     # Update the distances to what they'll be during the next collision
-    small.position += small.velocity*t
-    large.position += large.velocity*t
-    # Update collision count AND velocities to post-collision values
-    collisionCount = updateVelocities(small, large, collisionCount)
+    position[0] += velocity[0]*t
+    position[1] += velocity[1]*t
+            
+    # Update collision count and velocities to post-collision values
+    if(position == [0, 0]):
+      # Extra collision counted when both blocks simultaneously reach the wall
+      # and bounce off it and each other
+      collisionCount += 1
+    collisionCount += 1
+    updateVelocities(mass, velocity, position)
+    
+    # Limit the value that the numerator and denominator can take to prevent
+    # them from blowing up and slowing down the program. Thanks to Quuxplusone
+    # (https://codereview.stackexchange.com/users/16369/quuxplusone) for
+    # suggesting this
+    position[0] = position[0].limit_denominator(2**32)
+    position[1] = position[1].limit_denominator(2**32)
+    velocity[0] = velocity[0].limit_denominator(2**32)
+    velocity[1] = velocity[1].limit_denominator(2**32)
+  print("--------------------------\nTotal number of collisions:", 
+        collisionCount)
   return collisionCount
 
-def main(m1, m2, v1, v2, x1, x2):
-  print(simulate(Block(m1, v1, x2), Block(m2, v2, x2)))
-
-main(1, 10000, 0, -7, 4, 8)
+if __name__ == "__main__":
+  simulate()
